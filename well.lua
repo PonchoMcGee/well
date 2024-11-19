62fa38a617757d55873e82d3897ee83bdf682366
PonchoMcGee <104921914+PonchoMcGee@users.noreply.github.com>
2024-11-19 16:44:01 -0700

    Update well.lua

--- well.lua
+++ well.lua
@@ -1,122 +1,74 @@
 -- Well
 -- A deep audio well 
 -- with circular ripples
--- v1.0.3 @PonchoMcGee
+-- v1.0.4 @your_name
+--
+-- E1: Change scale
+-- E2: Direction (Up/Down)
+-- E3: Speed/Hold time
+-- K2: Toggle mode
+-- K3: Toggle sound source
+-- Grid: Play + hold notes
 
 engine.name = 'PolyPerc'
 
 -- requirements
 musicutil = require 'musicutil'
 
--- initialization
-function init()
-  -- initialize script state
-  params:add_group("WELL", 13)
+-- declare variables at file level
+local g = grid.connect()
+local grid_dirty = false
+local grid_ripples = {}
+local held_notes = {}
+local screen_dirty = true
+local show_instructions = true
+local mode = 1
+local screen_center_x = 64
+local screen_center_y = 32
+local circle_spacing = 8
+local num_circles = 6
+local hold_metro
+local screen_metro
+local grid_metro
+
+-- grid key function (must be declared before init)
+function grid.key(x, y, z)
+  local pos = x .. "," .. y
   
-  params:add_option("scale", "Scale", {"Major", "Minor", "Pentatonic", "Chromatic"}, 1)
-  params:add_option("direction", "Direction", {"Down", "Up"}, 1)
-  params:add_control("echo_spacing", "Echo Speed", controlspec.new(0.05, 1.0, 'lin', 0.01, 0.2, "s"))
-  params:add_control("hold_interval", "Hold Time", controlspec.new(0.1, 2.0, 'lin', 0.01, 0.25, "s"))
-  params:add_control("decay_factor", "Decay", controlspec.new(0.1, 0.99, 'lin', 0.01, 0.8))
-  params:add_option("sound_source", "Sound Source", {"PolyPerc", "Sample"}, 1)
-  params:add_number("base_note", "Base Note", 0, 127, 60)
-  
-  params:add_control("cutoff", "Cutoff", controlspec.new(50, 5000, 'exp', 0, 1000, "Hz"))
-  params:add_control("release", "Release", controlspec.new(0.1, 3.0, 'lin', 0, 0.5, "s"))
-  
-  params:bang()
-  
-  -- visual state
-  screen_dirty = true
-  show_instructions = true
-  mode = 1 -- 1 for main, 2 for secondary parameters
-  
-  -- held notes state
-  held_notes = {}
-  
-  -- visual parameters
-  screen_center_x = 64
-  screen_center_y = 32
-  circle_spacing = 8
-  num_circles = 6
-  
-  -- initialize grid
-  g = grid.connect()
+  if z == 1 then
+    -- create new ripple
+    local ripple = {
+      x = x,
+      y = y,
+      radius = 1,
+      life = 8
+    }
+    table.insert(grid_ripples, ripple)
+    
+    -- store held note
+    held_notes[pos] = {x = x, y = y}
+    
+    -- play the note
+    play_note(x, y)
+    
+    -- start metro if this is the first held note
+    if tab.count(held_notes) == 1 and hold_metro then
+      hold_metro:start()
+    end
+    
+  else -- z == 0
+    -- remove held note
+    held_notes[pos] = nil
+    
+    -- stop metro if no more held notes
+    if tab.count(held_notes) == 0 and hold_metro then
+      hold_metro:stop()
+    end
+  end
   grid_dirty = true
-  grid_ripples = {}
-  
-  -- initialize softcut for sample playback
-  for i=1,6 do
-    softcut.enable(i, 1)
-    softcut.buffer(i, 1)
-    softcut.level(i, 1.0)
-    softcut.position(i, 1)
-    softcut.loop(i, 1)
-    softcut.loop_start(i, 1)
-    softcut.loop_end(i, 5)
-    softcut.play(i, 1)
-    softcut.rate(i, 1.0)
-    if i == 1 then
-      softcut.rec(i, 1)
-      softcut.level_input_cut(1, i, 1.0)
-      softcut.level_input_cut(2, i, 1.0)
-    end
-  end
-  
-  -- initialize PolyPerc parameters
-  engine.release(params:get("release"))
-  engine.cutoff(params:get("cutoff"))
-  
-  -- initialize metro for held notes
-  hold_metro = metro.init()
-  hold_metro.event = play_held_notes
-  hold_metro.time = params:get("hold_interval")
-  
-  -- metro for screen redraw
-  screen_metro = metro.init()
-  screen_metro.event = function()
-    if screen_dirty then
-      redraw()
-      screen_dirty = false
-    end
-  end
-  screen_metro.time = 1/15
-  screen_metro:start()
-  
-  -- metro for grid redraw
-  grid_metro = metro.init()
-  grid_metro.event = function()
-    if grid_dirty then
-      grid_redraw()
-      grid_dirty = false
-    end
-  end
-  grid_metro.time = 1/30
-  grid_metro:start()
-  
-  -- load default sample
-  softcut.buffer_clear()
-  softcut.buffer_read_mono(_path.audio.."common/cricket.wav", 0, 1, -1, 1, 1)
 end
 
--- cleanup on script close
-function cleanup()
-  hold_metro:stop()
-  screen_metro:stop()
-  grid_metro:stop()
-  
-  -- clear softcut
-  softcut.reset()
-end
-
--- function to play held notes
-function play_held_notes()
-  for pos, note in pairs(held_notes) do
-    play_note(note.x, note.y)
-  end
-end
-
--- function to play a note
+-- helper function for note playing
 function play_note(x, y)
   local note_offset = ((8-y) * 4) + (x-8)
   local note = params:get("base_note") + note_offset
@@ -139,6 +91,90 @@ function play_note(x, y)
   else -- PolyPerc
     engine.hz(musicutil.note_num_to_freq(note))
   end
+end
+
+-- function to play held notes
+function play_held_notes()
+  for pos, note in pairs(held_notes) do
+    play_note(note.x, note.y)
+  end
+end
+
+-- initialization
+function init()
+  -- script state params
+  params:add_group("WELL", 13)
+  params:add_option("scale", "Scale", {"Major", "Minor", "Pentatonic", "Chromatic"}, 1)
+  params:add_option("direction", "Direction", {"Down", "Up"}, 1)
+  params:add_control("echo_spacing", "Echo Speed", controlspec.new(0.05, 1.0, 'lin', 0.01, 0.2, "s"))
+  params:add_control("hold_interval", "Hold Time", controlspec.new(0.1, 2.0, 'lin', 0.01, 0.25, "s"))
+  params:add_control("decay_factor", "Decay", controlspec.new(0.1, 0.99, 'lin', 0.01, 0.8))
+  params:add_option("sound_source", "Sound Source", {"PolyPerc", "Sample"}, 1)
+  params:add_number("base_note", "Base Note", 0, 127, 60)
+  params:add_control("cutoff", "Cutoff", controlspec.new(50, 5000, 'exp', 0, 1000, "Hz"))
+  params:add_control("release", "Release", controlspec.new(0.1, 3.0, 'lin', 0, 0.5, "s"))
+  params:bang()
+  
+  -- initialize softcut for sample playback
+  softcut.reset()
+  
+  for i=1,6 do
+    softcut.enable(i, 1)
+    softcut.buffer(i, 1)
+    softcut.level(i, 1.0)
+    softcut.position(i, 1)
+    softcut.loop(i, 1)
+    softcut.loop_start(i, 1)
+    softcut.loop_end(i, 5)
+    softcut.play(i, 1)
+    softcut.rate(i, 1.0)
+    if i == 1 then
+      softcut.rec(i, 1)
+      softcut.level_input_cut(1, i, 1.0)
+      softcut.level_input_cut(2, i, 1.0)
+    end
+  end
+  
+  -- initialize PolyPerc parameters
+  engine.release(params:get("release"))
+  engine.cutoff(params:get("cutoff"))
+  
+  -- initialize metros
+  hold_metro = metro.init()
+  hold_metro.event = play_held_notes
+  hold_metro.time = params:get("hold_interval")
+  
+  screen_metro = metro.init()
+  screen_metro.event = function()
+    if screen_dirty then
+      redraw()
+      screen_dirty = false
+    end
+  end
+  screen_metro.time = 1/15
+  screen_metro:start()
+  
+  grid_metro = metro.init()
+  grid_metro.event = function()
+    if grid_dirty then
+      grid_redraw()
+      grid_dirty = false
+    end
+  end
+  grid_metro.time = 1/30
+  grid_metro:start()
+  
+  -- load default sample
+  softcut.buffer_clear()
+  softcut.buffer_read_mono(_path.audio.."common/cricket.wav", 0, 1, -1, 1, 1)
+end
+
+-- cleanup on script close
+function cleanup()
+  if hold_metro then hold_metro:stop() end
+  if screen_metro then screen_metro:stop() end
+  if grid_metro then grid_metro:stop() end
+  softcut.reset()
 end
 
 -- screen redraw function
@@ -198,47 +234,8 @@ function redraw()
   screen.update()
 end
 
--- grid key function
-function g.key(x, y, z)
-  local pos = x .. "," .. y
-  
-  if z == 1 then
-    -- create new ripple
-    local ripple = {
-      x = x,
-      y = y,
-      radius = 1,
-      life = 8
-    }
-    table.insert(grid_ripples, ripple)
-    
-    -- store held note
-    held_notes[pos] = {x = x, y = y}
-    
-    -- play the note
-    play_note(x, y)
-    
-    -- start metro if this is the first held note
-    if tab.count(held_notes) == 1 then
-      hold_metro:start()
-    end
-    
-  else -- z == 0
-    -- remove held note
-    held_notes[pos] = nil
-    
-    -- stop metro if no more held notes
-    if tab.count(held_notes) == 0 then
-      hold_metro:stop()
-    end
-  end
-  grid_dirty = true
-end
-
 -- grid redraw function
 function grid_redraw()
-  if g == nil then return end
-  
   g:all(0)
   
   -- draw held notes
@@ -296,7 +293,9 @@ function enc(n,d)
       params:delta("echo_spacing", d)
     else
       params:delta("hold_interval", d)
-      hold_metro.time = params:get("hold_interval")
+      if hold_metro then
+        hold_metro.time = params:get("hold_interval")
+      end
     end
   end
   screen_dirty = true
